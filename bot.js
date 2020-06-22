f84d0e6baca55a4934b64e437fc9a00f65a883ea
zhidaothecoder <46723925+zhidaothecoder@users.noreply.github.com>
2020-06-22 13:04:46 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on('message', message => {
 
     if (message.content === 'ping') {
 
-       message.reply('pong');
+       message.reply('pong pong pong pong pong pong pong pong pong');
 
        }
 
